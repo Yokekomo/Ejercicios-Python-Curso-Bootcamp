63e99d9a6924509a8a60e29e93cde4c77fa95a83
Albano <77249799+Yokekomo@users.noreply.github.com>
2022-02-16 08:19:35 +0100

    Ejercicio tema 2 Python OB

---
+++ Ejercicio_Tema_2.py
@@ -0,0 +1,11 @@
+
+"""
+En este ejercicio tendrás que crear un archivo python donde tendrás que crear una variable a
+la cual asignarás una cadena de texto y la imprimirás por consola.
+Por otro lado, tendrás que modificar la variable y después imprimirla por consola para ver
+la modificación de la variable.
+"""
+
+cadena = 'Hola soy una cadena de texto'
+
+print(f'{cadena} modificada')
